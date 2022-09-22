fa057bbc6590001ace893352736b2b2c204bdcba
Dries Costermans <dries.costermans@student.kuleuven.be>
2022-09-22 16:02:48 +0200

    Add tables

--- exercise2-files/setup.py
+++ exercise2-files/setup.py
@@ -8,11 +8,44 @@ class Setup:
         self.db_connection = self.connection.db_connection
         self.cursor = self.connection.cursor
 
-    def create_tables(self):
-        pass
+    def create_user_table(self):
+        query = """ CREATE TABLE User(
+                    id varchar(15) PRIMARY KEY
+                    has_labels boolean
+                    )"""
+        self.cursor.execute(query)
+        self.db_connection.commit()
+
+    def create_activity_table(self):
+        query = """ CREATE TABLE Activity(
+                    id integer PRIMARY KEY 
+                    user_id varchar(15) 
+                    transportation_mode varchar(15) 
+                    start_date_time datetime
+                    end_date_time datetime
+                    FOREIGN KEY (user_id) REFERENCES (User(id))
+                    )"""
+        self.cursor.execute(query)
+        self.db_connection.commit()
+
+    def create_trackpoint_table(self):
+        query = """ CREATE TABLE TrackPoint(
+                    id integer PRIMARY KEY
+                    activity_id integer 
+                    lat double 
+                    lon double
+                    altitude integer 
+                    date_days double
+                    date_time datetime
+                    FOREIGN KEY (activity_id) REFERENCES (Activity(id))
+                    )"""
+        self.cursor.execute(query)
+        self.db_connection.commit()
+
 
     def insert_data(self):
         pass
+
 
 
 def main():
